133e1fc01e7cac67aab45aa6a027d2272678da25
Cristian Perez <cperezlecaros@gmail.com>
2023-11-30 22:29:32 -0700

    Creation of parent class View, searchView and resultsView to display search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,10 +1,11 @@
 import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
-const recipeContainer = document.querySelector(".recipe");
+import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
 
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async () => {
   try {
@@ -22,8 +23,26 @@ const controlRecipes = async () => {
   }
 };
 
+const controlSearchResults = async () => {
+  try {
+    resultsView.renderSpinner();
+    //1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    //2) Load search results
+    await model.loadSearchResults(query);
+    //Render results
+
+    resultsView.render(model.state.search.results);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -3,11 +3,15 @@ import { getJSON } from "./helpers.js";
 
 export const state = {
   recipe: {},
+  search: {
+    query: "",
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
     const { recipe } = data.data;
 
     state.recipe = {
@@ -22,6 +26,24 @@ export const loadRecipe = async function (id) {
     };
   } catch (error) {
     //error temporal
+    console.log(`${error} `);
+    throw error;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    state.search.results = data.data.recipes.map((rec) => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+  } catch (error) {
     console.log(`${error} `);
     throw error;
   }
